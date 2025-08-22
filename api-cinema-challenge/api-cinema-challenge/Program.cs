501a9f05e73eebb282c3bf2aec2c0a69c485037b
Jakub Mroz <kuba.mroz01@gmail.com>
2025-08-22 09:35:04 +0200

    feat: initial classes

--- api-cinema-challenge/api-cinema-challenge/Program.cs
+++ api-cinema-challenge/api-cinema-challenge/Program.cs
@@ -1,10 +1,16 @@
 using api_cinema_challenge.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<CinemaContext>();
 
 var app = builder.Build();
@@ -12,9 +18,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Demo API");
+    });
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+// endpoints configuration
+
 app.Run();
